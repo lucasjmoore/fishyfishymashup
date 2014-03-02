220a58f3c8de0d5ef7d980f1216d9ab780fe174c
summerspirit <Naomi@Naomis-MacBook-Air.local>
2014-03-02 12:07:05 -0500

    Deleting bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ FishyFishy::Application.routes.draw do
   resources :scrapers
 
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
